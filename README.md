adb8089157fc54c357fb543ddae2781d49d29d6d
sentinel_red <sentinel_red@live.com>
2021-01-04 17:59:13 +0000

    Updating

---
+++ README.md
@@ -0,0 +1,2 @@
+# acalvo
+antonio calvo repo
--- UserClasses/UserClass.cs
+++ UserClasses/UserClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace User_API.UserClasses
 {
     public class UserClass
     {
+        [Key]
         public String Password { get; set; }
 
+        [Required]
+        //Validation rule
+        [MaxLength (50, ErrorMessage = "Name cannot be that long")]
         public String UserName { get; set; }
 
+        [StringLength(200, ErrorMessage = "Message cannot be that long")]
         public String Message { get; set; }
 
 
